c4940c515ce10538be3f69534dbca5ddb045df82
kyle <khughessean01@gmail.com>
2024-01-07 12:23:30 +0200

    nft collection whitelist contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,12 @@
 const hre = require("hardhat");
 
+// Cur bal => 0.5098 SepoliaETH before remix interaction
+/*
+  Observations 
+  1. If gas fee is > than withdrawal amount then -0 on metamask trx list 
+  2. Withdrawal amount was consumed in order to cover gas fee. So, trx fees are deducted from the receive amount
+*/
+
 async function main() {
   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
   const unlockTime = currentTimestampInSeconds + 60;
