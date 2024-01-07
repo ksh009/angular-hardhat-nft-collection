e7edf721ebb51fb2df92b8481431563218aa33e3
kyle <khughessean01@gmail.com>
2024-01-07 12:44:14 +0200

    deploy script => etherscan api required

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,43 @@
 const hre = require("hardhat");
 
+async function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function main() {
+  /*
+    DeployContract in ethers.js is an abstraction used to deploy new smart contracts,
+    so whitelistContract here is a factory for instances of our Whitelist contract.
+    */
+  // here we deploy the contract
+  // [10] - Should constructor accept multiple args on init this array will accept them.
+  const whitelistContract = await hre.ethers.deployContract("Whitelist", [10]);
+  // 10 is the Maximum number of whitelisted addresses allowed
+
+  // wait for the contract to deploy
+  await whitelistContract.waitForDeployment();
+
+  // print the address of the deployed contract
+  console.log("Whitelist Contract Address:", whitelistContract.target);
+
+  // Sleep for 30 seconds while Etherscan indexes the new contract deployment
+  await sleep(30 * 1000); // 30s = 30 * 1000 milliseconds
+
+  // Verify the contract on etherscan
+  await hre.run("verify:verify", {
+    address: whitelistContract.target,
+    constructorArguments: [10],
+  });
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
 // Cur bal => 0.5098 SepoliaETH before remix interaction
 /*
   Observations 
@@ -7,29 +45,29 @@ const hre = require("hardhat");
   2. Withdrawal amount was consumed in order to cover gas fee. So, trx fees are deducted from the receive amount
 */
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+// async function main() {
+//   const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+//   const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+//   const lockedAmount = hre.ethers.parseEther("0.001");
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+//   const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+//     value: lockedAmount,
+//   });
 
-  await lock.waitForDeployment();
-  const txHash = lock.deploymentTransaction().hash;
+//   await lock.waitForDeployment();
+//   const txHash = lock.deploymentTransaction().hash;
 
-  console.log(
-    `Lock with ${hre.ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${
-      lock.target
-    }. Transaction at https://sepolia.etherscan.io/tx/${txHash}`
-  );
-}
+//   console.log(
+//     `Lock with ${hre.ethers.formatEther(
+//       lockedAmount
+//     )}ETH and unlock timestamp ${unlockTime} deployed to ${
+//       lock.target
+//     }. Transaction at https://sepolia.etherscan.io/tx/${txHash}`
+//   );
+// }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// main().catch((error) => {
+//   console.error(error);
+//   process.exitCode = 1;
+// });
